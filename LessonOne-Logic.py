dbaa8ba3278f8e9f3d9038ae7339cf9f699c2654
robm1158 <mullins097956@gmail.com>
2020-08-09 00:21:00 -0600

    Intial Commit

---
+++ LessonOne-Logic.py
@@ -0,0 +1,8 @@
+# Here we will learn about basic logic and what theses logical statements can do 
+# in a basid sense. But first we will want to start you off as every new programmer
+# should learning something new! A hello world program!
+
+# First off make sure you are doing these things in your own files as this will provide
+# as a templete.
+
+print("Hellow World")
